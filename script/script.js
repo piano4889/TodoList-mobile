ec213589fac44ab1087345cdaa024ab1006a0cdd
dhKim <vksah602@naver.com>
2023-09-11 15:48:38 +0900

    Scroll -> Drag 구현 방법 변경 container 한 개로 축소

--- script/script.js
+++ script/script.js
@@ -1,51 +1,102 @@
-function generateCalendar(year, month) {
-	// 현재 달의 1일과 마지막 날짜를 구함
-	const firstDay = new Date(year, month, 1);
-	const lastDay = new Date(year, month + 1, 0);
-	const daysInMonth = lastDay.getDate();
-
-	// 현재 달의 1일의 요일을 구함 (0: 일요일, 1: 월요일, ..., 6: 토요일)
-	const firstDayOfWeek = firstDay.getDay();
-
-	// 현재 달의 이름을 가져오고, 해당 월의 HTML 요소를 선택
-	const monthName = firstDay.toLocaleString('en', { month: 'long' });
-	const monthElement = document.querySelector(`.${monthName.toLowerCase()}`);
-	const weeksContainer = monthElement.querySelectorAll('ul[class^="week"]');
-
-	let currentDay = 1;
-	let prevMonthDay = (new Date(year, month, 0)).getDate() - firstDayOfWeek + 1;
-
-	// 각 주(week)에 날짜를 추가
-	weeksContainer.forEach((week, index) => {
-		week.innerHTML = '';
-
-		for (let i = 0; i < 7; i++) {
-			// 첫 주의 경우 이전 달의 마지막 주 날짜를 추가하며, 첫 주의 시작 요일을 채움
-			if ((index === 0 && i < firstDayOfWeek) || currentDay > daysInMonth) {
-				const dayLi = document.createElement('li');
-				dayLi.innerHTML = `<span>${prevMonthDay}</span>`;
-				week.appendChild(dayLi);
-
-				prevMonthDay++;
-			} else {
-				// 현재 달의 날짜를 추가
-				const dayLi = document.createElement('li');
-				dayLi.textContent = currentDay;
-				week.appendChild(dayLi);
-
-				currentDay++;
-			}
-		}
-	});
-}
-
-// 페이지 로드 시 실행되는 함수
 document.addEventListener('DOMContentLoaded', () => {
 	// 오늘 날짜 정보를 가져옴
 	const today = new Date();
+	let currentYear = today.getFullYear();
+	let currentMonth = today.getMonth();
+
+	// 현재 달력을 생성
+	generateCalendar(currentYear, currentMonth);
+
+	// 슬라이드 컨테이너에 드래그 이벤트 추가
+	const sliderContainer = document.querySelector('.TodoTemplate');
+	let touchStartX = 0;
+	let touchEndX = 0;
+
+	sliderContainer.addEventListener('touchstart', (e) => {
+		touchStartX = e.touches[0].clientX;
+	});
+
+	sliderContainer.addEventListener('touchend', (e) => {
+		touchEndX = e.changedTouches[0].clientX;
+
+		const deltaX = touchEndX - touchStartX;
+		const threshold = 50; // 드래그 인식 임계값
+
+		if (deltaX > threshold) {
+			// 오른쪽으로 드래그 (이전 달)
+			currentMonth--;
+			if (currentMonth < 0) {
+				currentMonth = 11;
+				currentYear--;
+			}
+		} else if (deltaX < -threshold) {
+			// 왼쪽으로 드래그 (다음 달)
+			currentMonth++;
+			if (currentMonth > 11) {
+				currentMonth = 0;
+				currentYear++;
+			}
+		}
+
+		// 달력을 업데이트
+		generateCalendar(currentYear, currentMonth);
+	});
+});
+
+
+function generateCalendar(year, month) {
+	// 현재 날짜 정보 가져오기
+	const today = new Date();
 	const currentYear = today.getFullYear();
 	const currentMonth = today.getMonth();
+	const currentDate = today.getDate();
 
-	// 현재 달의 달력을 생성
-	generateCalendar(currentYear, currentMonth);
-});
+	// 캘린더 생성 로직
+	const firstDay = new Date(year, month, 1);
+	const lastDay = new Date(year, month + 1, 0);
+	const daysInMonth = lastDay.getDate();
+	const firstDayOfWeek = firstDay.getDay();
+	const monthName = firstDay.toLocaleString('en', { month: 'long' });
+	const monthElement = document.querySelector('.current__month');
+	const weeksContainer = monthElement.querySelectorAll('ul[class^="week"]');
+
+	let currentDay = 1;
+	let prevMonthDay = new Date(year, month, 0).getDate() - firstDayOfWeek + 1;
+
+	// 현재 월 이름을 캘린더에 설정
+	monthElement.querySelector('h1').textContent = monthName;
+
+	// 주(week)를 생성하고 날짜를 추가하는 함수
+	function createWeek(week, weekIndex) {
+		week.innerHTML = '';
+
+		for (let i = 0; i < 7; i++) {
+			const dayLi = document.createElement('li');
+
+			if ((weekIndex === 0 && i < firstDayOfWeek) || currentDay > daysInMonth) {
+				// 이전 달의 날짜나 다음 달의 날짜를 추가
+				dayLi.innerHTML = `<span>${prevMonthDay}</span>`;
+				dayLi.classList.add('prev-month');
+				prevMonthDay++;
+			} else {
+				// 현재 달의 날짜를 추가
+				dayLi.textContent = currentDay;
+				if (year === currentYear && month === currentMonth && currentDay === currentDate) {
+					// 오늘 날짜에 .today 클래스 추가
+					dayLi.classList.add('today');
+				}
+				currentDay++;
+			}
+			week.appendChild(dayLi);
+		}
+	}
+
+	// 각 주(week)에 날짜를 추가
+	weeksContainer.forEach((week, index) => {
+		createWeek(week, index);
+	});
+}
+
+
+
+
